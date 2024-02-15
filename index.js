fac55f86f3d3b34879ae942062bbe28a0e84e086
Sladjana Dejanovic <csl.bunny.2207@gmail.com>
2024-02-15 21:22:25 +0100

    articles and user

--- index.js
+++ index.js
@@ -8,23 +8,35 @@ const app = express()
 const port = 3000;
 
 app.use(express.static("public"));
+app.set("view engine", "ejs") // setting ejs as express app view engine ..
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const user = {
+  firstName: 'Sladjana',
+  lastName: 'Dejanovic',
+  admin: true
+}
+
+const posts = [
+  {title: 'Title 1', body: 'Body 1' },
+  {title: 'Title 2', body: 'Body 2' },
+  {title: 'Title 3', body: 'Body 3' },
+  {title: 'Title 4', body: 'Body 4' },
+]
+
+app.use(bodyParser.urlencoded({ extended: true })); //change this, body parser isnt workign anymore
 
 app.listen(port, ()=>{
   console.log(`Server running on port ${port}`);
 })
 
 app.get("/", (req, res)=>{
-  res.render("index.ejs")
+  res.render("index", {user}) //.. so here we dont need to provide extention for index.ejs, bc app already knows what to look for
 })
 
-app.post("/create", (req, res)=>{
-
-
-  res.render("hello world")
+app.get("/create", (req, res)=>{
+  res.render("partials/create")
 })
 
-app.get("/testbutton", (req, res)=>{
-  res.render("partials/test.ejs")
+app.get("/articles", (req,res)=>{
+  res.render("partials/articles", {articles:posts})
 })
