75353cda3994a8f57a88c4aa67bc020e176bc48f
Sladjana Dejanovic <csl.bunny.2207@gmail.com>
2024-02-18 23:22:46 +0100

    logging

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser"; // change?
-// import {dirname} from "path";
-// import { fileURLToPath } from "url";
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+import session from "express-session"
+
 
 const app = express()
 const port = 3000;
@@ -10,17 +9,42 @@ const port = 3000;
 app.use(express.static("public"));
 app.set("view engine", "ejs") // setting ejs as express app view engine ...
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: true
+}))
 
-const user = {
-  firstName: 'Sladjana',
+const users = [
+  {
+     firstName: 'Sladjana',
   lastName: 'Dejanovic',
   email: "sladja@gmail.com",
   password: "1234",
   admin: true
+},
+  {
+    firstName: 'Yuri',
+  lastName: 'Cruz Franca',
+  email: "yuri@gmail.com",
+  password: "1234",
+  admin: true
+},
+  {
+    firstName: 'Azrael',
+  lastName: 'Dejanovic',
+  email: "azibazi@gmail.com",
+  password: "1234",
+  admin: false
 }
+]
+
+// let user;
+// let loggedIn = false;
+
 
 let postIdCounter = 4;
-console.log(postIdCounter);
+
 let posts = [
   {title: 'Title 1', content: 'Body 1' },
   {title: 'Title 2', content: 'Body 2' },
@@ -29,9 +53,33 @@ let posts = [
 ]
 
 
-app.get("/", (req, res)=>{//.. so here we dont need to provide extention for index.ejs, bc app already knows what to look for
-  res.render("pages/index", {user,
+app.get("/", (req, res)=>{
+  const user = req.session.user
+  if (user) {
+    res.render("pages/index", {user,
   title: "Home page"}) 
+   
+  } 
+
+  
+})
+
+app.post("/login", (req,res)=>{
+  const {email, password}=req.body
+const user = users.find(user => user.email===email && user.password===password)
+
+if(user){
+   // Store user information in session
+   req.session.user = user;
+}else{
+  res.status(401).send("Invalid email or password");
+}
+})
+
+// Render navbar with "create post" button based on user's admin status
+app.use((req, res, next)=>{
+  res.locals.user = req.session.user // Make user data available in templates
+  next()
 })
 
 app.get("/create", (req, res)=>{
@@ -77,13 +125,6 @@ app.get("/about", (req,res)=>{
   res.render("pages/about", {user, title: "About"})
 })
 
-app.post("/login", (req,res)=>{
-  const {email, password}=req.body
-
-  if(email===user.email && password===user.password){
-    res.redirect("/")
-  }
-})
 
 app.listen(port, ()=>{
   console.log(`Server running on port ${port}`);
